a300dc99fcd3e1c1de714a3187aa176258c3f090
Jakub Radek <jakub4426@gmail.com>
2023-05-23 00:00:24 +0200

    Revision of gui screen, code refactoring, texture generation revision, highlight when hover implementation

--- src/constatnts.py
+++ src/constatnts.py
@@ -1,6 +1,6 @@
 POINT_SIDE_SIZE = 50  # [m]
-CELL_SIDE_SIZE = 10
-GRID_SIDE_SIZE = 10
+CELL_SIDE_SIZE = 10  # points per cell side size
+GRID_SIDE_SIZE = 10  # cells per grid side size
 WORLD_SIDE_SIZE = CELL_SIDE_SIZE * GRID_SIDE_SIZE
 ITER_AS_SEC = 20
 
