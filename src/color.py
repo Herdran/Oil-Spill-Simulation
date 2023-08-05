441348a2013b657bec0c84d663e2b212229fe6f7
Mapet <jakubsordyl1@gmail.com>
2023-08-05 15:18:36 +0200

    refactor

--- src/color.py
+++ src/color.py
@@ -1,6 +1,6 @@
 COLOR_RANGE_MAX = 255.0
 
-def rgba(r, g, b, a=1.0):
+def rgba(r, g, b, a=255):
     into_color_range = lambda x: min(max(x / COLOR_RANGE_MAX, 0.0), 1.0)
     return [into_color_range(x) for x in [r, g, b, a]]
 
