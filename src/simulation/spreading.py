e7d3d7c093b70fc63d2a8e0d53fca95160b14b50
Mapet <jakubsordyl1@gmail.com>
2023-08-20 18:24:20 +0200

    Cr corrections + refactor

--- src/simulation/spreading.py
+++ src/simulation/spreading.py
@@ -2,10 +2,24 @@ from typing import Dict
 from math import exp, sqrt
 from random import random as rand
 
-from simulation.point import InitialValues, Point, Coord_t, TopographyState
+from simulation.point import Point, Coord_t, TopographyState
 import constatnts as const
 
 
+'''
+There are 4 spreading turns. Each turn is responsible for spreading oil with neighbour in one direction.
+Here is a filter thet check if the point is the first ponit of spreading pair (is left or top of the pair).
+
+So for example if points are:
+00 01 02 03
+10 11 12 13
+20 21 22 23
+
+at first turn processed points are: (00, 01), (02, 03), (10, 11)...
+at second: (01, 02), (11, 12), (21, 22)...
+at third: (00, 10), (01, 11), (02, 12) ...
+at fourth: (10, 20), (11, 21), (12, 22)...
+'''
 SPREADING_PAIRS_FILTERS = [
     lambda coord: coord[0] % 2 == 0,
     lambda coord: coord[0] % 2 == 1,
@@ -13,6 +27,9 @@ SPREADING_PAIRS_FILTERS = [
     lambda coord: coord[1] % 2 == 1,
 ]
 
+'''
+Offset of the second point of spreading pair from the first one.
+'''
 SPREADING_NEXT = [
     (1, 0),
     (1, 0),
@@ -32,36 +49,41 @@ class Spreading_engine:
         self.world = engine.world
         self.engine = engine
     
-    def spread_oil_points(self, total_mass, delta_time: float):
+    def spread_oil_points(self, total_mass: float, delta_time: float):
         for spreading_turn in range(len(SPREADING_PAIRS_FILTERS)):
             new_points = {}
-            for key in self.world.keys():
-                if SPREADING_PAIRS_FILTERS[spreading_turn](key):
-                    second = spreading_next(spreading_turn, key)
-                    if second not in self.world:
-                        second_point = self.new_point(second)
-                        new_points[second] = second_point
-                    else:
-                        second_point = self.world[second]
-                    self.process_spread_between(total_mass, delta_time, self.world[key], second_point) 
-                    continue
-                prev = spreading_prev(spreading_turn, key)
-                if prev not in self.world:
-                    prev_point = self.new_point(prev)
-                    new_points[prev] = prev_point
-                    self.process_spread_between(total_mass, delta_time, prev_point, self.world[key])
+            for coord in self.world.keys():
+                if SPREADING_PAIRS_FILTERS[spreading_turn](coord):
+                    second_point = self.get_spreading_next(spreading_turn, new_points, coord)
+                    self.process_spread_between(total_mass, delta_time, self.world[coord], second_point) 
+                else:
+                    prev = spreading_prev(spreading_turn, coord)
+                    if prev not in self.world:
+                        prev_point = self.new_point(prev, new_points)
+                        self.process_spread_between(total_mass, delta_time, prev_point, self.world[coord])
             self.world.update(new_points)
+
+    def get_spreading_next(self, spreading_turn: int, new_points: Dict[Coord_t, Point], coord: Coord_t) -> Point:
+        second = spreading_next(spreading_turn, coord)
+        if second in self.world:
+            return self.world[second]
+        return self.new_point(second, new_points)
     
+    def new_point(self, coord: Coord_t, new_points: Dict[Coord_t, Point]) -> Point:
+        point = Point(coord, self.initial_values, self.engine)
+        new_points[coord] = point
+        return point
+        
     def process_spread_between(self, total_mass: float, delta_time: float, first: Point, second: Point) -> None:
         if not (first.topography == TopographyState.SEA and second.topography == TopographyState.SEA):
             return
 
         length = const.POINT_SIDE_SIZE
         V = total_mass / self.initial_values.density
-        g = 9.8
+        G = 9.8
         delta = (self.initial_values.water_density - self.initial_values.density) / self.initial_values.water_density
         viscosity = 10e-6
-        D = 0.49 / self.initial_values.propagation_factor * (V ** 2 * g * delta / sqrt(viscosity)) ** (1 / 3) / sqrt(
+        D = 0.49 / self.initial_values.propagation_factor * (V ** 2 * G * delta / sqrt(viscosity)) ** (1 / 3) / sqrt(
             delta_time)
         delta_mass = 0.5 * (second.oil_mass - first.oil_mass) * (1 - exp(-2 * D / (length ** 2) * delta_time))
 
@@ -81,9 +103,6 @@ class Spreading_engine:
         to_cell.viscosity = (to_cell.viscosity * to_cell.oil_mass + from_cell.viscosity * real_delta) / (
                 to_cell.oil_mass + real_delta)
         to_cell.emulsification_rate = (
-                                              to_cell.emulsification_rate * to_cell.oil_mass + from_cell.emulsification_rate * real_delta) / (
-                                              to_cell.oil_mass + real_delta)
+            to_cell.emulsification_rate * to_cell.oil_mass + from_cell.emulsification_rate * real_delta) / (
+            to_cell.oil_mass + real_delta)
         to_cell.oil_mass += real_delta
-        
-    def new_point(self, coord: Coord_t) -> Point:
-        return Point(coord, self.initial_values, self.engine)
