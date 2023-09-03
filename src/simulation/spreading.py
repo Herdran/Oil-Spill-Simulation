a6670c65c03a3147bbf8cf6da2a9314caeeeecb5
bblachut1 <bogojedi1@gmail.com>
2023-09-03 22:02:02 +0200

    fix spreading bug

--- src/simulation/spreading.py
+++ src/simulation/spreading.py
@@ -1,10 +1,9 @@
-from typing import Dict
 from math import exp, sqrt
 from random import random as rand
+from typing import Dict, Optional
 
-from simulation.point import Point, Coord_t, TopographyState
 import constatnts as const
-
+from simulation.point import Point, Coord_t, TopographyState
 
 '''
 There are 4 spreading turns. Each turn is responsible for spreading oil with neighbour in one direction.
@@ -37,43 +36,52 @@ SPREADING_NEXT = [
     (0, 1),
 ]
 
+
 def spreading_next(turn: int, coord: Coord_t) -> Coord_t:
     return coord[0] + SPREADING_NEXT[turn][0], coord[1] + SPREADING_NEXT[turn][1]
 
+
 def spreading_prev(turn: int, coord: Coord_t) -> Coord_t:
-    return coord[0] - SPREADING_NEXT[turn][0], coord[1] - SPREADING_NEXT[turn][1] 
+    return coord[0] - SPREADING_NEXT[turn][0], coord[1] - SPREADING_NEXT[turn][1]
+
 
 class Spreading_engine:
     def __init__(self, engine):
         self.initial_values = engine.initial_values
         self.world = engine.world
         self.engine = engine
-    
+
     def spread_oil_points(self, total_mass: float, delta_time: float):
         for spreading_turn in range(len(SPREADING_PAIRS_FILTERS)):
             new_points = {}
             for coord in self.world.keys():
                 if SPREADING_PAIRS_FILTERS[spreading_turn](coord):
                     second_point = self.get_spreading_next(spreading_turn, new_points, coord)
-                    self.process_spread_between(total_mass, delta_time, self.world[coord], second_point) 
+                    if second_point is not None:
+                        self.process_spread_between(total_mass, delta_time, self.world[coord], second_point)
                 else:
                     prev = spreading_prev(spreading_turn, coord)
+                    if not (0 <= prev[0] < const.POINTS_SIDE_COUNT and 0 <= prev[1] < const.POINTS_SIDE_COUNT):
+                        continue
                     if prev not in self.world:
                         prev_point = self.new_point(prev, new_points)
                         self.process_spread_between(total_mass, delta_time, prev_point, self.world[coord])
             self.world.update(new_points)
 
-    def get_spreading_next(self, spreading_turn: int, new_points: Dict[Coord_t, Point], coord: Coord_t) -> Point:
+    def get_spreading_next(self, spreading_turn: int, new_points: Dict[Coord_t, Point], coord: Coord_t) ->\
+            Optional[Point]:
         second = spreading_next(spreading_turn, coord)
+        if not (0 <= second[0] < const.POINTS_SIDE_COUNT and 0 <= second[1] < const.POINTS_SIDE_COUNT):
+            return None
         if second in self.world:
             return self.world[second]
         return self.new_point(second, new_points)
-    
+
     def new_point(self, coord: Coord_t, new_points: Dict[Coord_t, Point]) -> Point:
         point = Point(coord, self.initial_values, self.engine)
         new_points[coord] = point
         return point
-        
+
     def process_spread_between(self, total_mass: float, delta_time: float, first: Point, second: Point) -> None:
         if not (first.topography == TopographyState.SEA and second.topography == TopographyState.SEA):
             return
@@ -103,6 +111,6 @@ class Spreading_engine:
         to_cell.viscosity = (to_cell.viscosity * to_cell.oil_mass + from_cell.viscosity * real_delta) / (
                 to_cell.oil_mass + real_delta)
         to_cell.emulsification_rate = (
-            to_cell.emulsification_rate * to_cell.oil_mass + from_cell.emulsification_rate * real_delta) / (
-            to_cell.oil_mass + real_delta)
+                to_cell.emulsification_rate * to_cell.oil_mass + from_cell.emulsification_rate * real_delta) / (
+                to_cell.oil_mass + real_delta)
         to_cell.oil_mass += real_delta
