2a2d419ad533fb1fcc2389c1b1b22946417e69a3
Mapet <jakubsordyl1@gmail.com>
2023-11-29 19:50:45 +0100

    fix convert binary script - better treshold value

--- data/world_map/convert_script.py
+++ data/world_map/convert_script.py
@@ -3,31 +3,31 @@ import os
 from PIL import Image
 import numpy as np
 
+DATA_PATH = 'data/world_map/original'
+SAVE_PATH = 'data/world_map'
+
+
 def get_save_file_name(original_path):
     return os.path.basename(original_path) + '.bin'
 
-def convert_tif_to_binary(file_path, threshold):
+
+def convert_tif_to_binary(file_path):
     img = Image.open(file_path)
-    img_gray = img.convert('L')
-    img_array = np.array(img_gray)
-    binary_array = np.where(img_array <= threshold, 1, 0)
+    img_array = np.array(img)
+    binary_array = np.where(img_array < 0, 1, 0)
     binary_bytes = np.packbits(binary_array)
 
-    with open(get_save_file_name(file_path), 'wb') as f:
-        f.write(binary_bytes)
+    with open(os.path.join(SAVE_PATH, get_save_file_name(file_path)), 'wb') as f:
+       f.write(binary_bytes)
 
 
 if __name__ == '__main__':
     Image.MAX_IMAGE_PIXELS = 999999999
     
-    MAX_GRAY_VALUE = 255
-    TRESHOLD = MAX_GRAY_VALUE / 2
-    
-    DATA_PATH = 'data/world_map/original'
-    
     for file in os.listdir(DATA_PATH):
-        if file.endswith('.tif'):
+        if file.endswith('.tifx'):
             file = os.path.join(DATA_PATH, file)
             print(f'Converting {file}...')
-            convert_tif_to_binary(file, TRESHOLD)
+            convert_tif_to_binary(file)
             print(f'Converted {file} -> {get_save_file_name(file)}')
+            
