f1fbc7c7d4318784e6fd16828bda75b06b6fc101
Mapet <jakubsordyl1@gmail.com>
2023-12-02 12:21:43 +0100

    create log directory if missing

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,11 @@ from time import strftime
 
 from files import get_log_config_path, get_log_output_path
 
-def init_logger():
+def get_logger_output_path():
     date_str = strftime("%Y-%m-%d_%H-%M-%S")
-    logger_output_path = get_log_output_path().joinpath(f"log_{date_str}.logs")
-    config.fileConfig(get_log_config_path(), defaults={'logfilename': str(logger_output_path)})
+    logger_path = get_log_output_path()
+    logger_path.mkdir(parents=True, exist_ok=True)
+    return logger_path.joinpath(f"log_{date_str}.logs")
+
+def init_logger():
+    config.fileConfig(get_log_config_path(), defaults={'logfilename': str(get_logger_output_path())})
