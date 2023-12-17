f4cec8d44bc08aeaa03c43cfc1af210cd4f02e1a
Mapet <jakubsordyl1@gmail.com>
2023-12-17 21:17:17 +0100

    remove not needed parameter

--- src/gui/parameters_screen.py
+++ src/gui/parameters_screen.py
@@ -798,10 +798,7 @@ def start_initial_menu(window):
             offset_after_last_weather_minutes = total_simulation_time_minutes % self.data_time_step_minutes
             data_preprocessor_initial_seconds = self.total_simulation_time - (offset_after_last_weather_minutes * SECONDS_IN_MINUTE) + total_simulation_time_second_offset
             data_preprocessor_initial_timestamp = pd.Timestamp(self.time_range_start) + pd.Timedelta(seconds=data_preprocessor_initial_seconds)
-            InitialValues.data_preprocessor_initial_timestamp = data_preprocessor_initial_timestamp 
-            
-            InitialValues.data_preprocessor_total_time_offset = self.total_simulation_time
-            
+            InitialValues.data_preprocessor_initial_timestamp = data_preprocessor_initial_timestamp             
 
         def crop_and_resize_preview_image(self, event=None):
             image_width, image_height = self.loaded_img.size
